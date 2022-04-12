805a66421b8dc9fb70cfc081602358435884a8e2
jeyhunt <social.jeykurniawan@gmail.com>
2022-04-12 20:29:52 +0700

    added feature register and login admin/user

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,16 @@
 const userController = require("../controllers/userController");
+const { auth, keepLogin, resetPassword } = require("../helper/authToken");
 const router = require("express").Router();
 
 router.post("/register", userController.addUser);
+router.patch("/verified", auth, userController.verifUser);
+router.post("/login", userController.getUser);
+router.post("/keep-login", keepLogin, userController.userKeepLogin);
+router.post("/forgot-password", userController.forgotPasswordUser);
+router.patch(
+  "/reset-password",
+  resetPassword,
+  userController.resetPasswordUser
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const bearerToken = require("express-bearer-token");
 const PORT = process.env.PORT || 5000;
 
 // create instance
@@ -7,16 +8,20 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(bearerToken());
+app.use(express.static("public"));
 
 // routers
 const productRouter = require("./routers/productRouter");
 const categoryRouter = require("./routers/categoryRouter");
 const userRouter = require("./routers/userRouter");
+const adminRouter = require("./routers/adminRouter");
 
 // main routes
 app.use("/products", productRouter);
 app.use("/category", categoryRouter);
 app.use("/users", userRouter);
+app.use("/admin", adminRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "welcome to my api" });
