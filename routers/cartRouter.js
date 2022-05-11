31154f4805f44d5986466e6fc0f3c4055b27e1a4
Daffa <daffamrizqy@gmail.com>
2022-05-11 13:38:57 +0700

    done: backend cart functionality

--- routers/cartRouter.js
+++ routers/cartRouter.js
@@ -5,4 +5,12 @@ router.put("/", cartController.replaceCart);
 router.patch("/", cartController.updateCartRow);
 router.get("/", cartController.getCartByUserId);
 
+// daffa
+router.get('/user/:id', cartController.DFGetCartItems)
+router.post('/add-cart', cartController.DFAddcart)
+router.delete('/delete-cart/:id/user/:userId', cartController.DFDeleteCart)
+router.patch('/update/:id/user/:userId', cartController.DFUpdate)
+
+
+
 module.exports = router;
--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 const productController = require("../controllers/productController");
 const router = require("express").Router();
 
-router.post(
-  "/add-product",
-  productController.upload,
-  productController.addProduct
-);
+
+router.post("/add-product", productController.upload, productController.addProduct);
 
 router.get("/get-all-products", productController.getAllProducts);
 
@@ -18,23 +14,8 @@ router.delete("/delete-product/:id", productController.deleteProduct);
 
 router.post("/restore-product/:id", productController.restoreProduct);
 
+
+// daffa
+router.get('/all-products', productController.DFAllProducts)
+
 module.exports = router;
-=======
-const productController = require('../controllers/productController')
-const router = require('express').Router()
-
-
-router.post('/add-product', productController.upload, productController.addProduct)
-
-router.get('/get-all-products', productController.getAllProducts)
-
-router.get('/get-product-byId/:id', productController.getProductById)
-
-router.patch('/update-product/:id', productController.updateProduct)
-
-router.delete('/delete-product/:id', productController.deleteProduct)
-
-router.post('/restore-product/:id', productController.restoreProduct)
-
-module.exports = router
->>>>>>> 0716545bb5bf6a1fb4a17efefdf88a9072d60940
